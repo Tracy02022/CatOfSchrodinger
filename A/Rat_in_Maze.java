30227e029df73cb117fe1e78ee9e385722d7af95
Peng <eamonpeng91@gmail.com>
2015-07-26 23:56:09 -0400

    Update Rat_in_Maze.java

--- A/Rat_in_Maze.java
+++ A/Rat_in_Maze.java
@@ -24,3 +24,122 @@ Else
    d) If none of the above solutions work then unmark this cell as 0 
        (BACKTRACK) and return false.
 */
+/*
+boolean return type if statement 条件判断成功即返回，不成功继续下面
+public class IFTest {
+	public static boolean iFTest(int x) {
+		if (x + 1 == 7) {
+			return false;
+		}
+		if (x - 1 == 5) {
+			return true;
+		}
+		if (x / 2 == 3) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(iFTest(6));
+	}
+}
+// output: false
+public class IFTest {
+	public static boolean iFTest(int x) {
+		boolean flag = false;
+		if (x + 1 == 7) {
+			return true;
+		}
+		if (x - 1 == 5) {
+			return true;
+		}
+		if (x / 2 == 3) {
+			return false;
+		}
+		return flag;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(iFTest(6));
+	}
+}
+// output: true
+public class IFTest {
+	public static String iFTest(int x) {
+		if (x + 1 == 7) {
+			System.out.print("Hello");
+		}
+		if (x - 1 == 5) {
+			System.out.print("UMass");
+		}
+		if (x / 2 == 3) {
+			System.out.print("Good");
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		iFTest(6);
+	}
+}
+// output: HelloUMassGood
+*/
+public class Rat_in_Maze {
+	
+	public static boolean solveMaze(int[][] grid) {
+
+		int rows = grid.length;
+		int cols = grid[0].length;
+		// boolean[][] visited = new boolean[rows][cols];
+		int[][] visited = new int[rows][cols];
+		
+		if (!dfsHelper(0, 0, grid, visited)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean dfsHelper(int i, int j, int[][] grid, int[][] visited) {
+		if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 9) {
+			visited[i][j] = 1;
+			return true;
+		}
+		
+		if (isSafe(grid, i, j) == true && visited[i][j] == 0) {
+			visited[i][j] = 1;
+			if (dfsHelper(i - 1, j, grid, visited)) {
+				return true;
+			} 
+			if (dfsHelper(i, j + 1, grid, visited)) {
+				return true;
+			} 
+			if (dfsHelper(i + 1, j, grid, visited)) {
+				return true;
+			}
+			if (dfsHelper(i, j - 1, grid, visited)) {
+				return true;
+			} 
+			visited[i][j] = 0;
+			return false;
+		}
+		return false;
+	}
+	
+	public static boolean isSafe(int[][] grid, int i, int j) {
+		if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[][] maze = {
+		                {1, 0, 1, 1, 1},
+		                {1, 1, 0, 0, 1},
+		                {0, 1, 0, 0, 1},
+		                {0, 9, 1, 1, 1}};
+		
+		System.out.print(solveMaze(maze));	
+	}
+}
