a5383c9b75a7f771e71a6e3abbed70d4c9277e00
Peng <Eamon4213@users.noreply.github.com>
2015-07-27 23:42:37 -0400

    Update Sorted_Insert.java

--- A/Sorted_Insert.java
+++ A/Sorted_Insert.java
@@ -2,3 +2,29 @@
 Sorted insert for circular linked list
 Write a function to insert a new value in a sorted Circular Linked List (CLL).
 */
+/*
+1) Linked List is empty:  
+    a)  since new_node is the only node in CLL, make a self loop.      
+          new_node->next = new_node;  
+    b) change the head pointer to point to new node.
+          *head_ref = new_node;
+2) New node is to be inserted just before the head node:    
+  (a) Find out the last node using a loop.
+         while(current->next != *head_ref)
+            current = current->next;
+  (b) Change the next of last node. 
+         current->next = new_node;
+  (c) Change next of new node to point to head.
+         new_node->next = *head_ref;
+  (d) change the head pointer to point to new node.
+         *head_ref = new_node;
+3) New node is to be inserted somewhere after the head: 
+   (a) Locate the node after which new node is to be inserted.
+         while ( current->next!= *head_ref && 
+             current->next->data < new_node->data)
+         {   current = current->next;   }
+   (b) Make next of new_node as next of the located pointer
+         new_node->next = current->next;
+   (c) Change the next of the located pointer
+         current->next = new_node; 
+*/
