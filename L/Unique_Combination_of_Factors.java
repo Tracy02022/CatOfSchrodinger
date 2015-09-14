dbe2f85ab131ef5a93839bf27bf5a22faa5f962c
Peng <Eamon4213@users.noreply.github.com>
2015-09-14 16:31:45 -0400

    Update Unique_Combination_of_Factors.java

--- L/Unique_Combination_of_Factors.java
+++ L/Unique_Combination_of_Factors.java
@@ -2,4 +2,12 @@
 Unique Combination of Factors
 打印一个数的所有乘数组合，从大到小，不要有重复
 Print all unique combinations of factors of a positive integer. For example given 24:
+24*1
+12*2
+8*3
+6*4
+6*2*2
+4*3*2
+3*2*2*2
 */
+
